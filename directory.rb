3eaa05f0812e31057102af0d5f7f3d32fd59673d
Victoria Jusko <vikjusko@gmail.com>
2020-08-18 08:19:37 +0100

    Ineractive menu added

--- directory.rb
+++ directory.rb
@@ -1,28 +1,61 @@
+@students = []
+
 def input_students
   puts "Please enter the names of students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
-students = input_students
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again please"
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(students)
-  students.each do |student|
+
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
-print_header
-print(students)
-print_footer(students)
+
+interactive_menu
