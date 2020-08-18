ed341c925e97a3af9293baec44929ac3e9eead02
Victoria Jusko <vikjusko@gmail.com>
2020-08-18 08:32:23 +0100

    Students file added

--- directory.rb
+++ directory.rb
@@ -21,6 +21,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list of students.csv"
   puts "9. Exit"
 end
 
@@ -30,12 +31,24 @@ def show_students
   print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when "1"
     input_students
   when "2"
     show_students
+  when "3"
+   save_students
   when "9"
     exit
   else
